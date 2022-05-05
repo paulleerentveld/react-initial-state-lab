df87231270f6cd167dca131145d01cc1d97575be
paulleerentveld <paul@veldnet.com.au>
2022-05-05 16:30:02 +1000

    lab stuff

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+    constructor(props) {
+        super();
+
+        this.state = {
+            secondsLeft : props.initialCount
+        }
+      }
+
+    decrementSecondsLeft() {
+        this.setState({
+            secondsLeft: this.state.secondsLeft  -1
+        })
+    }
+
+  render() {
+    return (
+      <div>
+          {/* <button onClick={() => this.decrementSecondsLeft()} >Decrement Time</button> */}
+          {this.state.secondsLeft <=0 ? "Boom!" : this.state.secondsLeft + " seconds left before I go boom!" } 
+      </div>
+    )
+  }
+
+}
